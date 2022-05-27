35519633383b469af650fefc71e4468079506d35
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-05-27 02:24:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = (["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function writeCards(names) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  };
+
+  return (messages);
+};
+
+
+
+function countDown() {
+  let count = 11;
+  while (count > 0) {
+    count--;
+    console.log(count);
+  };
+};
